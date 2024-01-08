8828c7e60ac50da6b64e38e872f1fe39db05bed1
ALLEWAN3 <allen.al.wang@dbschenker.com>
2024-01-08 13:32:06 +0800

    chore:

--- src/providers/comment.ts
+++ src/providers/comment.ts
@@ -1,23 +1,20 @@
-import { httpProvider } from './http'
+import { httpProvider } from "./http";
 
 export class CommentProvider {
-  /**
-   * 获取所有帖子
-   */
   static async addComment(params: {
-    entityId: string
-    entityType: IEntityType
-    content: string
+    entityId: string;
+    entityType: IEntityType;
+    content: string;
   }) {
-    return httpProvider.post('/comment/add', params)
+    return httpProvider.post("/comment/add", params);
   }
 
   static async getCommentList(params: {
-    pageNum: number,
-      pageSize: number,
-      entityId: string,
-      entityType: IEntityType,
+    pageNum: number;
+    pageSize: number;
+    entityId: string;
+    entityType: IEntityType;
   }): Promise<PagerList<IComment>> {
-    return httpProvider.post('comment/list', params)
+    return httpProvider.post("comment/list", params);
   }
 }
