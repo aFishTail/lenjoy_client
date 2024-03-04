123d312b8457a4db9ebaa35c702f657cc2291796
wangheng3 <wangheng3@360.cn>
2024-03-04 21:04:16 +0800

    添加百度统计代码

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script dangerouslySetInnerHTML={{__html: `
+        var _hmt = _hmt || [];
+        (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?c96f37da9833c4126de27500e6c95177";
+        var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();`}}
+      />
+      </Head>
       <body>
         <Main />
         <NextScript />
