5797b429ba31d71820525fdc2bde1a005f93cd09
afishtail <1765174487@qq.com>
2024-05-30 07:28:02 +0800

    fix: baidu verify

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <script dangerouslySetInnerHTML={{__html: `
+        <meta name="baidu-site-verification" content="codeva-PoOXymScZA" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
         var _hmt = _hmt || [];
         (function() {
         var hm = document.createElement("script");
         hm.src = "https://hm.baidu.com/hm.js?c96f37da9833c4126de27500e6c95177";
         var s = document.getElementsByTagName("script")[0]; 
           s.parentNode.insertBefore(hm, s);
-        })();`}}
-      />
+        })();`,
+          }}
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
