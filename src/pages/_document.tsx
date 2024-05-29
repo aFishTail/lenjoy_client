ebfeea7b15649e69cafa06d2c64699801056be7e
afishtail <1765174487@qq.com>
2024-05-30 07:33:38 +0800

    fix: baidu verify

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="baidu-site-verification" content="codeva-PoOXymScZA" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
